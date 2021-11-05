87ffdd4bcdc0e491c33046bc1651767040740cc5
azertypow <azertypow@gmail.com>
2021-11-05 16:50:29 +0100

    build: V0.1

---
+++ docs/precache-manifest.c443ac2ba6004f3c12c747e5e17bc97e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3a67c3e2509e5b3d4d",
+    "url": "/css/about.8b5fefc0.css"
+  },
+  {
+    "revision": "887822bcc39c0bf4cf6e",
+    "url": "/css/app.e153a4b0.css"
+  },
+  {
+    "revision": "6a59fec00b906fabb100655cdf2306bb",
+    "url": "/img/logo-silene.6a59fec0.jpg"
+  },
+  {
+    "revision": "49758c466471a1c797e34dfbff2c4da6",
+    "url": "/img/silene-illu--1.49758c46.jpg"
+  },
+  {
+    "revision": "01e82277628016baef44d46ca5bc7d6a",
+    "url": "/img/silene-illu--2.01e82277.jpg"
+  },
+  {
+    "revision": "071540fe8a39ef156fdd3733da2bca83",
+    "url": "/img/silene-illu--3.071540fe.jpg"
+  },
+  {
+    "revision": "0d498ae00a520791ee8fbfe02090e3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3a67c3e2509e5b3d4d",
+    "url": "/js/about.5f9e59a5.js"
+  },
+  {
+    "revision": "887822bcc39c0bf4cf6e",
+    "url": "/js/app.35cddb47.js"
+  },
+  {
+    "revision": "89d13af7fe7ffc589875",
+    "url": "/js/chunk-vendors.953372d0.js"
+  },
+  {
+    "revision": "70d0b33c74e84fb468c174fa83d678cf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

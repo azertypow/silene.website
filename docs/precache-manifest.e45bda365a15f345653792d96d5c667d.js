80917ac847bf28f632fdb6528af009949c5b0e3e
azertypow <azertypow@gmail.com>
2021-11-05 17:12:42 +0100

    build: V0.1.3

---
+++ docs/precache-manifest.e45bda365a15f345653792d96d5c667d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3a67c3e2509e5b3d4d",
+    "url": "https://azertypow.github.io/silene.website/css/about.8b5fefc0.css"
+  },
+  {
+    "revision": "89f6433a354b81fc6447",
+    "url": "https://azertypow.github.io/silene.website/css/app.515b5750.css"
+  },
+  {
+    "revision": "6a59fec00b906fabb100655cdf2306bb",
+    "url": "https://azertypow.github.io/silene.website/img/logo-silene.6a59fec0.jpg"
+  },
+  {
+    "revision": "49758c466471a1c797e34dfbff2c4da6",
+    "url": "https://azertypow.github.io/silene.website/img/silene-illu--1.49758c46.jpg"
+  },
+  {
+    "revision": "01e82277628016baef44d46ca5bc7d6a",
+    "url": "https://azertypow.github.io/silene.website/img/silene-illu--2.01e82277.jpg"
+  },
+  {
+    "revision": "071540fe8a39ef156fdd3733da2bca83",
+    "url": "https://azertypow.github.io/silene.website/img/silene-illu--3.071540fe.jpg"
+  },
+  {
+    "revision": "e5af3fb1df6f29720b4b739b2a5df13b",
+    "url": "https://azertypow.github.io/silene.website/index.html"
+  },
+  {
+    "revision": "da3a67c3e2509e5b3d4d",
+    "url": "https://azertypow.github.io/silene.website/js/about.5f9e59a5.js"
+  },
+  {
+    "revision": "89f6433a354b81fc6447",
+    "url": "https://azertypow.github.io/silene.website/js/app.3f518b6b.js"
+  },
+  {
+    "revision": "89d13af7fe7ffc589875",
+    "url": "https://azertypow.github.io/silene.website/js/chunk-vendors.953372d0.js"
+  },
+  {
+    "revision": "70d0b33c74e84fb468c174fa83d678cf",
+    "url": "https://azertypow.github.io/silene.website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://azertypow.github.io/silene.website/robots.txt"
+  }
+]);
